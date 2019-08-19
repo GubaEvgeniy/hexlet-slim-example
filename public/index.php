d01c4f1d29c7162af13aab918f40fb06744b8b75
gubaevgeniy <evgeniyguba@gmail.com>
2019-08-19 16:25:36 +0300

    Faker

--- public/index.php
+++ public/index.php
@@ -5,10 +5,14 @@ require __DIR__ . '/../vendor/autoload.php';
 
 use Slim\Factory\AppFactory;
 
+$companies = App\Generator::generate(100);
+
 $app = AppFactory::create();
 $app->addErrorMiddleware(true, true, true);
 
 $app->get('/', function ($request, $response) {
     return $response->write('Welcome to Slim!');
 });
+
+
 $app->run();
