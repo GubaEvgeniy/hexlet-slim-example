a6aa41095f5808dae12b452f918c98352cc6f4c3
gubaevgeniy <evgeniyguba@gmail.com>
2019-08-19 16:47:49 +0300

    Router and add lib collect()

--- public/index.php
+++ public/index.php
@@ -46,4 +46,20 @@ $app->get('/companies', function ($request, $response) use ($companies) {
     return $response->write(json_encode($company_per_page));
 });
 
+/**
+ * Router https://www.slimframework.com/docs/v3/objects/router.html
+ */
+$app->get('/companies/{id}', function ($request, $response, $args) use ($companies) {
+
+    $id = $args['id'];
+    $company = collect($companies)->firstWhere('id', '=', $id);
+
+    if (empty($company)) {
+        return $response->withStatus(404)->write('Page not found');
+    }
+
+    return $response->write(json_encode($company));
+});
+
+
 $app->run();
