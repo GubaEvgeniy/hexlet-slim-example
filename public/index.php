f713980efee2b03c57aaca2fbe5a574f7711f5b1
gubaevgeniy <evgeniyguba@gmail.com>
2019-08-20 11:53:00 +0300

    templates

--- public/index.php
+++ public/index.php
@@ -4,62 +4,33 @@
 require __DIR__ . '/../vendor/autoload.php';
 
 use Slim\Factory\AppFactory;
+use DI\Container;
 
-
-$companies = \App\Generator::generate(100);
-
+$container = new Container();
+$container->set('renderer', function () {
+    // Параметром передается базовая директория в которой будут храниться шаблоны
+    return new \Slim\Views\PhpRenderer(__DIR__ . '/../templates');
+});
+AppFactory::setContainer($container);
 $app = AppFactory::create();
 $app->addErrorMiddleware(true, true, true);
 
-$app->get('/', function ($request, $response) {
-    return $response->write('Welcome to Slim!');
-});
-/**
- *  Request handler
- */
-$domains = [];
-for ($i = 0; $i < 10; $i++) {
-    $domains[] = $companies[$i]['name'];
-}
+$app->get('/', function ($request, $response, $args) {
 
-$phones = [];
-for ($i = 0; $i < 10; $i++) {
-    $phones[] = $companies[$i]['phone'];
-}
-$app->get('/phones', function ($request, $response) use ($phones) {
-    return $response->write(json_encode($phones));
-});
-$app->get('/domains', function ($request, $response) use ($domains) {
-    return $response->write(json_encode($domains));
+    $title = 'Добро пожаловать';
+
+    $params = [
+        'title' => $title
+    ];
+
+    return $this->get('renderer')->render($response, 'homepage.phtml', $params);
 });
 
-/**
- * HTTP session
- */
-$app->get('/companies', function ($request, $response) use ($companies) {
-
-    $page = $request->getQueryParam('page', 1);
-    $per = $request->getQueryParam('per', 5);
-
-    $company_per_page = array_slice($companies, $per * ($page - 1), $per);
-
-    return $response->write(json_encode($company_per_page));
+$app->get('/users/{id}', function ($request, $response, $args) {
+    $params = ['id' => $args['id']];
+    // Указанный путь считается относительно базовой директории для шаблонов, заданной на этапе конфигурации
+    // $this доступен внутри анонимной функции благодаря http://php.net/manual/ru/closure.bindto.php
+    return $this->get('renderer')->render($response, 'users/show.phtml', $params);
 });
-
-/**
- * Router https://www.slimframework.com/docs/v3/objects/router.html
- */
-$app->get('/companies/{id}', function ($request, $response, $args) use ($companies) {
-
-    $id = $args['id'];
-    $company = collect($companies)->firstWhere('id', '=', $id);
-
-    if (empty($company)) {
-        return $response->withStatus(404)->write('Page not found');
-    }
-
-    return $response->write(json_encode($company));
-});
-
 
 $app->run();
